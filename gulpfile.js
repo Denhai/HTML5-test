ecb5de7e9c2fbdb4c0b9bd38d9501969db7742ba
Hayden <haybartlett@gmail.com>
2016-10-17 18:50:02 +1030

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var bs = require('browser-sync')
+
+var paths = {
+  html: 'src/**/*.html',
+  styles: 'src/styles/**/*.scss',
+  scripts: 'src/scripts/**/*.js'
+}
+
+gulp.task('browser-sync', ['html', 'styles'], function () {
+  bs({
+    server: {
+      baseDir: "./dist"
+    }
+  })
+})
+
+gulp.task('bs-reload', function () {
+  bs.reload()
+})
+
+gulp.task('styles', function () {
+  gulp.src(paths.styles)
+    .pipe(sass())
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(bs.reload({ stream: true }))
+})
+
+gulp.task('html', function() {
+  gulp.src(paths.html)
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch(paths.styles, ['styles'])
+  // gulp.watch(paths.scripts, ['scripts'])
+  gulp.watch(paths.html, ['html'])
+})
